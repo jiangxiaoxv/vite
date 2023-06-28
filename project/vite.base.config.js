6279de17196ccaaa9943ff2bb99c1014af4ab89f
jiangxiaoxv <1043479536@qq.com>
2023-06-28 11:43:32 +0800

    feat: 6 vite 配置文件代码提示，环境区分

---
+++ project/vite.base.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  optimizeDeps: {
+    // exclude: ['lodash-es'], // 当遇到lodash-es这个依赖的时候不进行依赖预构建，不处理多包传输问题
+  },
+});
--- project/vite.config.js
+++ project/vite.config.js
@@ -1,6 +1,27 @@
 import { defineConfig } from 'vite';
-export default defineConfig({
-  optimizeDeps: {
-    // exclude: ['lodash-es'], // 当遇到lodash-es这个依赖的时候不进行依赖预构建，不处理多包传输问题
+import viteBaseConfig from './vite.base.config';
+import vitePrdConfig from './vite.prd.config';
+import viteDevConfig from './vite.dev.config';
+
+// 策略模式
+const envResolver = {
+  build: () => {
+    console.log('生产');
+    return Object.assign({}, viteBaseConfig, vitePrdConfig);
   },
+  serve: () => {
+    console.log('开发');
+    return Object.assign({}, viteBaseConfig, viteDevConfig);
+  },
+};
+// defineConfig 有语法提示,ts语法
+export default defineConfig(({ command }) => {
+  // 是build还是serve主要取决于我们的命令是开发环境还是生产环境
+  console.log('command', command);
+  /* if (command === 'build') {
+    // 代表生成环境的配置
+  } else {
+    // 代表开发环境的配置
+  } */
+  return envResolver[command]();
 });
